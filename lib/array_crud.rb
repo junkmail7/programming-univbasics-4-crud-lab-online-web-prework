7a8c1b2be676b6af05df9869d9a092be8d47bd8c
William Pannell <lpann5@gmail.com>
2019-10-02 11:03:59 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -30,17 +30,22 @@ end
 def retrieve_element_from_index(array, index_number)
   array=[0,1,2]
   index_number=1
-  
+  array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array=[0,1,2]
+  array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  array=[0,1,2]
+  array[-1]
 end
 
 def update_element_from_index(array, index_number, element)
-
+  array=[0,1,2]
+  index_number=1
+  element=3
+  array[index_number]=element
 end
