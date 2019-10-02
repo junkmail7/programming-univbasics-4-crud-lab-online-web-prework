5ad2904920cd49c92d32a58caa267acc250aa25c
William Pannell <lpann5@gmail.com>
2019-10-02 11:05:37 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -14,16 +14,16 @@ end
 
 def add_element_to_start_of_array(array, element)
   array=[]
-  array.unshift("arrays!")
+  array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  array=[0,1,2]
+  array=["arrays!"]
   array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  array=[0,1,2]
+  array=["wow"]
   array.shift
 end
 
